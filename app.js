0e81384040db4ae1dc6e35d91ab98d7729cdf2eb
Kelvis123456 <kelvisguerrero03@gmail.com>
2026-04-24 21:08:15 -0400

    Fix: puerto dinámico para Render

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.get('/', (req, res) => {
 module.exports = app;
 
 if (require.main === module) {
-  app.listen(3000, () => console.log('Servidor en puerto 3000'));
+  const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
 }
